890af81f22f29314f8ffd789978c77dd0a23ae77
ConradZhangcong <heisenberg0519@outlook.com>
2022-02-17 22:56:20 +0800

    146

---
+++ 100-200/0146/index.js
@@ -0,0 +1,49 @@
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function (capacity) {
+  this.capacity = capacity;
+  this.cache = new Map();
+};
+
+/**
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function (key) {
+  if (!this.cache.has(key)) return -1;
+
+  const temp = this.cache.get(key);
+  this.cache.delete(key);
+  this.cache.set(key, temp);
+  return temp;
+};
+
+/**
+ * @param {number} key
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function (key, value) {
+  if (this.cache.has(key)) {
+    this.cache.delete(key);
+  } else if (this.cache.size >= this.capacity) {
+    this.cache.delete(this.cache.keys().next().value);
+  }
+  this.cache.set(key, value);
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
+
+var obj = new LRUCache(2);
+obj.put("a", 1);
+obj.put("b", 2);
+obj.put("c", 3);
+console.log(obj.cache);
+console.log(obj.get("a"));
+console.log(obj.get("b"));
---
+++ 100-200/0146/index.md
@@ -0,0 +1,5 @@
+# 146. LRU 缓存
+
+`LRU(least recently used)` 最近最少使用缓存算法
+
+链接：https://leetcode-cn.com/problems/lru-cache
