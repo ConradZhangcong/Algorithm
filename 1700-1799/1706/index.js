9a81a6d3552c99c529adba4892f4b35667a3e048
ConradZhangcong <heisenberg0519@outlook.com>
2022-02-24 10:26:25 +0800

    1706

---
+++ 1700-1799/1706/index.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number[][]} grid
+ * @return {number[]}
+ */
+var findBall = function (grid) {
+  const m = grid.length;
+  const n = grid[0].length;
+
+  const answer = new Array(n).fill(-1);
+
+  // 每个球进入循环
+  for (let col = 0; col < n; col++) {
+    let index = col; // 起始位置
+    for (let row = 0; row < m; row++) {
+      if (grid[row][index] === 1) {
+        // 往右 右边一格也需要往右
+        if (grid[row][index + 1] === 1 && index + 1 < n) {
+          index++;
+        } else {
+          index = null;
+          break;
+        }
+      } else {
+        // 往左 左边一格也需要往左
+        if (grid[row][index - 1] === -1 && index - 1 >= 0) {
+          index--;
+        } else {
+          index = null;
+          break;
+        }
+      }
+    }
+    answer[col] = index === null ? -1 : index;
+  }
+
+  return answer;
+};
+
+console.log(
+  findBall([
+    [1, 1, 1, 1, 1, 1],
+    [-1, -1, -1, -1, -1, -1],
+    [1, 1, 1, 1, 1, 1],
+    [-1, -1, -1, -1, -1, -1],
+  ])
+);
---
+++ 1700-1799/1706/index.md
@@ -0,0 +1,3 @@
+# 1706. 球会落何处
+
+链接：https://leetcode-cn.com/problems/where-will-the-ball-fall
